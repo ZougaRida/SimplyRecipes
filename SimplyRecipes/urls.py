1464f72e66b2d39a491faa71e735ce62d63682c5
ZougaRida <96395950+R-zouga@users.noreply.github.com>
2025-01-08 17:44:17 +0100

    Modified the base urls file

--- SimplyRecipes/urls.py
+++ SimplyRecipes/urls.py
@@ -1,6 +1,18 @@
+from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from recipe.views import IndexView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("tags/", include("tag.urls")),
+    path("recipes/", include("recipe.urls")),
+    path("", IndexView.as_view(), name="index"),
 ]
+
+if not settings.TESTING:
+    urlpatterns = [
+        *urlpatterns,
+    ] + debug_toolbar_urls()
